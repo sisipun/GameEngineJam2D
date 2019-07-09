aba646c5b5e31c17ef9876ea1437c0234eef6291
Alexey <sisipun@gmail.com>
2019-07-09 20:44:02 +0300

    [love] - show player

---
+++ Love/player.lua
@@ -0,0 +1,25 @@
+Player = Object.extend(Object)
+
+function Player:new(x, y, width, height, speed, sprite)
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.speed = speed
+    self.sprite = sprite
+end
+
+function Player:update(dt)
+    if love.keyboard.isDown("right") then self.x = self.x + self.speed * dt end
+    if love.keyboard.isDown("left") then self.x = self.x - self.speed * dt end
+end
+
+function Player:draw()
+    local scaleX, scaleY = getImageScaleForNewDimensions(self.sprite, self.width, self.height)
+    love.graphics.draw(self.sprite, self.x, self.y, 0, scaleX, scaleY)
+end
+
+function getImageScaleForNewDimensions(image, newWidth, newHeight)
+    local currentWidth, currentHeight = image:getDimensions()
+    return (newWidth / currentWidth), (newHeight / currentHeight)
+end
