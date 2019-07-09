b99b47dfab3d00e710265abfa8d1c8e3785f0a89
Alexey <sisipun@gmail.com>
2019-07-09 20:44:02 +0300

    show player

--- Love/main.lua
+++ Love/main.lua
@@ -1,3 +1,12 @@
-function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+function love.load()
+    Object = require "object"
+    require "player"
+
+    playerSprite = love.graphics.newImage("assets/hero_single.png")
+
+    player = Player(100, 100, 60, 100, 100, playerSprite)
 end
+
+function love.update(dt) player:update(dt) end
+
+function love.draw() player:draw() end
