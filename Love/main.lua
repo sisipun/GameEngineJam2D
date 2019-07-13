043403afacd94c7b93cdd5876ddf5d87817defc7
Alexey <sisipun@gmail.com>
2019-07-13 14:39:16 +0300

    add camera logic + death logic

--- Love/main.lua
+++ Love/main.lua
@@ -1,8 +1,10 @@
 function love.load()
     Object = require "object"
+    require "entity"
     require "player"
     require "ground"
     require "utils"
+    require "groundRow"
 
     playerSprite = love.graphics.newImage("assets/hero_single.png")
     groundSprite = love.graphics.newImage("assets/ground_single.png")
@@ -10,15 +12,51 @@ function love.load()
     gameWidth = love.graphics.getWidth()
     gameHeight = love.graphics.getHeight()
 
-    player = Player(100, 100, 60, 100, 100, playerSprite)
-    ground = Ground(200, 200, 100, 100, groundSprite)
+    restart()
 end
 
-function love.update(dt) player:update(dt) end
+function love.update(dt)
+    player:update(dt)
+    camera.y = camera.y + (20 * dt)
+
+    for i, row in ipairs(groundRows) do
+        for i, ground in ipairs(row:getValues()) do
+            player:resolveCollision(ground)
+        end
+    end
+    if (camera.y + gameHeight > lastGroundRow:getY() + distanceBetweenRows) then
+        lastGroundRow = GroundRow(lastGroundRow:getY() + distanceBetweenRows,
+                                  groundRowSize, 50, 50)
+        table.insert(groundRows, lastGroundRow)
+    end
+    if (player:getY() > camera.y + gameHeight / 2) then
+        camera.y = player:getY() - gameHeight / 2
+    end
+    if (camera.y > player:getY()) then
+        restart()
+    end
+end
 
 function love.draw()
-    local scaleX, scaleY = getImageScaleForNewDimensions(background, gameWidth, gameHeight)
-    love.graphics.draw(background, 0, 0, 0, scaleX, scaleY)
-    ground:draw()
+    love.graphics.translate(-camera.x, -camera.y)
+    local scaleX, scaleY = getImageScaleForNewDimensions(background, gameWidth,
+                                                         gameHeight)
+    love.graphics.draw(background, camera.x, camera.y, 0, scaleX, scaleY)
+    for i, row in ipairs(groundRows) do row:draw() end
     player:draw()
+end
+
+function restart() 
+    player = Player(100, 100, 30, 50, 150, 200, playerSprite)
+
+    initGroundY = 300
+    groundRowSize = 15
+    distanceBetweenRows = 300
+    groundRows = {}
+    lastGroundRow = GroundRow(initGroundY, groundRowSize, 50, 50)
+    table.insert(groundRows, lastGroundRow)
+
+    camera = {}
+    camera.x = 0
+    camera.y = 0
 end
