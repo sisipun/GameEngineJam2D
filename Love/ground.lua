1658407267d159f29f1f575987a945278249c70c
Alexey <sisipun@gmail.com>
2019-07-13 14:38:34 +0300

    [love] - add entity superclass

--- Love/ground.lua
+++ Love/ground.lua
@@ -1,14 +1,5 @@
-Ground = Object.extend(Object)
+Ground = Entity.extend(Entity)
 
 function Ground:new(x, y, width, height, sprite)
-    self.x = x
-    self.y = y
-    self.width = width
-    self.height = height
-    self.sprite = sprite
-end
-
-function Ground:draw()
-    local scaleX, scaleY = getImageScaleForNewDimensions(self.sprite, self.width, self.height)
-    love.graphics.draw(self.sprite, self.x, self.y, 0, scaleX, scaleY)
+    Ground.super.new(self, x, y, width, height, sprite)
 end
