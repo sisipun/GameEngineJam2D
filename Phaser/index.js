8626689aa9ca9b441590964130129367e18d78ed
Alexey <sisipun@gmail.com>
2019-07-17 19:15:29 +0300

    [phaser] Player on the screen

--- Phaser/index.js
+++ Phaser/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname+'/index.html'));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+app.use(express.static('public'))
 
 app.listen(8080);
