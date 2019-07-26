0ad473ce354002de45bcb5157aa6045e41c4b10a
Alexey <sisipun@gmail.com>
2019-07-26 19:23:33 +0300

    [corona] - add ground

--- CoronaSDK/main.lua
+++ CoronaSDK/main.lua
@@ -2,6 +2,6 @@ local hero = display.newImageRect("assets/hero_single.png", 30, 50)
 hero.x = display.contentCenterX
 hero.y = display.contentCenterY
 
-local hero = display.newImageRect("assets/ground_single.png", 50, 50)
-hero.x = display.contentCenterX
-hero.y = display.contentCenterY + 100
+local ground = display.newImageRect("assets/ground_single.png", 50, 50)
+ground.x = display.contentCenterX
+ground.y = display.contentCenterY + 100
