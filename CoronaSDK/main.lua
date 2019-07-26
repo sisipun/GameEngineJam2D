54a04e79b93a41ad6c25129d12058f62d15b906b
Alexey <sisipun@gmail.com>
2019-07-26 19:23:02 +0300

    [corona] - add hero

--- CoronaSDK/main.lua
+++ CoronaSDK/main.lua
@@ -1,7 +1,7 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+local hero = display.newImageRect("assets/hero_single.png", 30, 50)
+hero.x = display.contentCenterX
+hero.y = display.contentCenterY
 
--- Your code here
+local hero = display.newImageRect("assets/ground_single.png", 50, 50)
+hero.x = display.contentCenterX
+hero.y = display.contentCenterY + 100
