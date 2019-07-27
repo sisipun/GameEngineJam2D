896c2be6b8dac53cb38b5c97976a3a54a31dc25e
Alexey <sisipun@gmail.com>
2019-07-27 13:56:51 +0300

    [corona] - move left right

--- CoronaSDK/main.lua
+++ CoronaSDK/main.lua
@@ -1,4 +1,6 @@
-local background = display.newImageRect("assets/background.png", display.contentWidth, display.contentHeight)
+local background = display.newImageRect("assets/background.png",
+                                        display.contentWidth,
+                                        display.contentHeight)
 background.x = display.contentCenterX
 background.y = display.contentCenterY
 
@@ -9,3 +11,31 @@ hero.y = display.contentCenterY
 local ground = display.newImageRect("assets/ground_single.png", 50, 50)
 ground.x = display.contentCenterX
 ground.y = display.contentCenterY + 100
+
+local physics = require("physics")
+physics.start()
+
+physics.addBody(ground, "static")
+physics.addBody(hero, "dynamic")
+
+motionX = 0
+
+function playerVelocity(event)
+    if (event.phase == "began") then
+        if event.x >= display.contentCenterX then
+            motionX = 1
+        elseif event.x <= display.contentCenterX then
+            motionX = -1
+        end
+    elseif (event.phase == "ended") then
+        motionX = 0
+    end
+end
+
+Runtime:addEventListener("touch", playerVelocity)
+
+local function moveHero(event)
+    hero.x = hero.x + motionX
+end
+
+Runtime:addEventListener("enterFrame", moveHero)
