27c69695209fc04b05c25e4670dd016fa8a8f81d
Alexey <sisipun@gmail.com>
2019-07-30 22:56:19 +0300

    [cocos] - add script to hero

---
+++ Cocos2d/assets/Script/Hero.js
@@ -0,0 +1,28 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        properties: {
+            // main character's jump height
+            jumpHeight: 0,
+            // main character's jump duration
+            jumpDuration: 0,
+            // maximal movement speed
+            maxMoveSpeed: 0,
+            // acceleration
+            accel: 0,
+        },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start () {
+
+    },
+
+    update (dt) {
+        this.node.x = this.node.x + 10 * dt
+    },
+});
